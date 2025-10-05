146d2985d3d8a1ded15e73814d4f9f92931d4716
L1ghtError <dark.man.i.1987@gmial.com>
2025-10-05 23:38:33 +0300

    generate ci script

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,16 +1,20 @@
+#include <gtest/gtest.h>
 #include "../math_operations.h"
-#include <cassert>
-#include <iostream>
 
-void test_add() {
-    assert(add(2, 3) == 5);
-    assert(add(-1, 1) == 0);
-    assert(add(0, 0) == 0);
-    assert(add(-5, -7) == -12);
-    std::cout << "All add() tests passed!" << std::endl;
+TEST(MathOperationsTest, AddPositiveNumbers) {
+    EXPECT_EQ(add(2, 3), 5);
 }
 
-int main() {
-    test_add();
-    return 0;
+TEST(MathOperationsTest, AddNegativeNumbers) {
+    EXPECT_EQ(add(-1, 1), 0);
+    EXPECT_EQ(add(-5, -7), -12);
+}
+
+TEST(MathOperationsTest, AddZero) {
+    EXPECT_EQ(add(0, 0), 0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
